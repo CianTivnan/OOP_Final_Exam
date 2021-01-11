745464ff2251fff52518284db6b68cef2d31573a
CianTivnan <cian.tivnan@yahoo.ie>
2021-01-11 10:07:40 +0000

    Task (f) complete

---
+++ OOP_Final_Exam/CurrentAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Final_Exam
+{
+    class CurrentAccount : Account
+    {
+        //properties
+        public double InterestRate { get; set; }
+
+        //constructors
+        public CurrentAccount() : base() { }
+
+        public CurrentAccount(string a, string b, double c, DateTime d) :base()
+        {
+            InterestRate = .03;
+        }
+
+
+
+
+        public override double CalculateInterest()
+        {
+            if (InterestDate.Year == DateTime.Today.Year)
+            {
+                return 0;
+            }
+
+            double interest = Balance * InterestRate;
+
+            InterestDate = DateTime.Now;
+            return interest;
+        }
+    }
+}
---
+++ OOP_Final_Exam/SavingsAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Final_Exam
+{
+    class SavingsAccount : Account
+    {
+        //properties
+        public double InterestRate { get; set; }
+
+        //constructors
+        public SavingsAccount() : base() { }
+
+        public SavingsAccount(string a, string b, double c, DateTime d) : base()
+        {
+            InterestRate = .06;
+        }
+
+
+
+
+        public override double CalculateInterest()
+        {
+            if(InterestDate.Year == DateTime.Today.Year)
+            {
+                return 0;
+            }
+
+            double interest = Balance * InterestRate;
+
+            InterestDate = DateTime.Now;
+            return interest;
+        }
+    }
+}
