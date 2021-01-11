d70cb0b6d2a8680004670dae4321a2506c3052dd
CianTivnan <cian.tivnan@yahoo.ie>
2021-01-11 09:51:36 +0000

    Task (d) complete

---
+++ OOP_Final_Exam/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Final_Exam
+{
+    public abstract class Account
+    {
+        //properties
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Balance { get; set; }
+
+        public DateTime InterestDate { get; set; }
+
+        //constructors
+        public Account() { }
+
+        public Account(string a, string b, double c, DateTime d) 
+        {
+            FirstName = a;
+            LastName = b;
+            Balance = c;
+            InterestDate = d;
+        }
+
+        //abstract method
+        public abstract double CalculateInterest();
+    }
+}
--- OOP_Final_Exam/MainWindow.xaml.cs
+++ OOP_Final_Exam/MainWindow.xaml.cs
@@ -12,7 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+/*
+    Author : Cian Tivnan S00198481
+    Github Repository Link : https://github.com/CianTivnan/OOP_Final_Exam
+    Date : 11/01/2021
+    Desc : WPF Bank Account Program, Exam
+*/
 namespace OOP_Final_Exam
 {
     /// <summary>
